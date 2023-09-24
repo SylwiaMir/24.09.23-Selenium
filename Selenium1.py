af0c6a5450874813041b6f275636c2756e4b5359
Sylwia <mirowska.sylwia1990@gmail.com>
2023-09-24 13:10:21 +0200

    4

--- Selenium1.py
+++ Selenium1.py
@@ -7,13 +7,22 @@ from selenium.common.exceptions import NoSuchElementException
 driver = webdriver.Chrome()
 driver.get('https://www.saucedemo.com/')
 
-username = driver.find_element('xpath', '//*[@id="user-name"]')
+try:
+    username = driver.find_element('xpath', '//*[@id="user-name"]')
+    print('po Xpath')
+except:
+    username = driver.find_element('id', 'user-name')
+    print('po id')
+
+username.clear()
 username.send_keys("standard_user")
 
 # password = driver.find_element('xpath', '//*[@id="password"]')
 # password.send_keys('secret_sauce')
 
 password = driver.find_element(By.XPATH, '//*[@id="password"]') #szukanie elementow z uzyciem By
+
+password.clear()
 password.send_keys('secret_sauce')
 
 login_button = driver.find_element('xpath','//*[@id="login-button"]')
